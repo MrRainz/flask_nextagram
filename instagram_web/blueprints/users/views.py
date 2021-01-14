bb345b5d394fa9c848f730544c14786bc0a84ff5
MrRainz <yue_sum_96@hotmail.com>
2021-01-14 12:52:50 +0800

    moved the validate function to user.py from views.py

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, flash, redirect, request, url_for
-from werkzeug.security import generate_password_hash
-import re
+
 from models.user import *
 
 users_blueprint = Blueprint('users',
@@ -15,51 +14,18 @@ def sign_up():
 
 @users_blueprint.route('/sign_up', methods=['POST'])
 def create():
-    valid_count = 0
     username = request.form['username']
     name = request.form['name']
     email = request.form['email']
     password = request.form['password']
     confirm_password = request.form['confirm_password']
-    if len(username) > 5 and len(username) < 21:
-        valid_count += 1
+    user = User(username=username, name=name, email=email, password=password, confirm_password=confirm_password)
+    if user.save():
+        flash("Registration successful!")
+        return redirect(url_for('home'))
     else:
-        flash("Username must be 6-20 characters")
-    if len(name) > 0 and len(name) < 51:
-        valid_count += 1
-    else:
-        flash("Display name must be 1-50 characters")
-    email_length = email.split("@")
-    if len(email_length[0]) > 3 and len(email_length[0]) < 20:
-        valid_count += 1
-    else:
-        flash("Email length needs to be 3-20 characters long")
-    if re.search('[^a-zA-Z0-9]', password) and len(password) > 7 and len(password) < 20:
-        valid_count += 1
-    else:
-        flash("Password needs to be 8-20 characters long and contains at least 1 special character.")
-    if confirm_password == password:
-        valid_count += 1
-    else:
-        flash("Passwords do not match.")
-    if valid_count == 5:
-        hash_password = generate_password_hash(password)
-        user = User(username=username, name=name, email=email, password=hash_password)
-        integrity_count = 0
-        if not User.get_or_none(User.username==username):
-            integrity_count += 1
-        else:
-            flash("Username taken!")
-        if not User.get_or_none(User.email==email):
-            integrity_count += 1
-        else:
-            flash("Email taken!")
-        if integrity_count == 2:
-            if user.save():
-                flash("Registration successful!")
-            else:
-                flash("Registration failed.")
-    return redirect(url_for('users.sign_up'))
+        flash(user.errors)
+        return redirect(url_for('users.sign_up'))
 
 
 @users_blueprint.route('/sign_in', methods=["GET"])
--- models/user.py
+++ models/user.py
@@ -1,12 +1,45 @@
 from models.base_model import BaseModel
 import peewee as pw
-
+import re
+from werkzeug.security import generate_password_hash
 
 class User(BaseModel):
     name = pw.CharField(unique=False)
     username = pw.CharField(unique=True)
     email = pw.CharField(unique=True)
-    password = pw.CharField(unique=False)
+    hash_password = pw.CharField(null=False)
+    password = None
+    confirm_password = None
 
-    
+    def validate(self):
+        existing_user_username = User.get_or_none(User.username == self.username)
+        if existing_user_username:
+            self.errors.append(f"{self.username} already taken!")
+        
+        existing_user_email = User.get_or_none(User.email == self.email)
+        if existing_user_email:
+            self.errors.append(f"{self.email} already taken!")
+        
+        if len(self.username) < 6 or len(self.username) > 20:
+            self.errors.append("Username needs to be 6-20 characters long.")
+        
+        if len(self.name) == 0 or len(self.name) > 50:
+            self.errors.append("Name needs to be 1-50 characters long.")
 
+        validate_email = self.email.split("@")
+        if len(validate_email[0]) < 4 and len(validate_email[0]) > 20:
+            self.errors.append("Email needs to be between 4-20 characters long.")
+
+        if len(self.password) < 8 and len(self.password) > 20:
+            self.errors.append("Password needs to be 8-20 characters and have uppercase, lowercase, and special characters.")
+
+        if self.password != self.confirm_password:
+            self.errors.append("Passwords do not match!")
+
+        has_lower = re.search(r"[a-z]", self.password)
+        has_upper = re.search(r"[A-Z]", self.password)
+        has_special = re.search(r"[^a-zA-Z0-9]", self.password)
+        if has_lower and has_upper and has_special:
+            self.hash_password = generate_password_hash(self.password)
+        else:
+            self.errors.append("Password needs to have at least 1 uppercase letter, 1 lower case letter, and 1 special character.")
