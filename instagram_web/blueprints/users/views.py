9979cc645d37ff780d8fa0030f3072505fe4284b
MrRainz <yue_sum_96@hotmail.com>
2021-01-19 18:16:05 +0800

    restructured HTML layout

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, flash, redirect, request, url_for
 from flask_login import login_user, logout_user, login_required, current_user
 from models.user import *
+from models.image import *
 from app import login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 import boto3, botocore
--- models/image.py
+++ models/image.py
@@ -1,5 +1,6 @@
 from models.base_model import BaseModel
 import peewee as pw
+from models.user import User
 
 class Image(BaseModel):
     user = pw.ForeignKeyField(User, backref='images')
